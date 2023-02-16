1a619f1cb3f38cf2f846a1bbe874802f58b208fa
Daniel207eae <elbotcrack@gmail.com>
2023-02-16 15:50:45 -0500

    Practica4-primercommit

---
+++ practica4.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+  
+// Driver code
+int main()
+{
+    int pid, pid1, pid2;
+
+    pid = fork();
+
+    if (pid == 0) {
+        int b=0;
+        for (int i = 0; i <= 5; i++)
+        {
+            b+=i;
+            printf("child[1] --> pid = %d and ppid = %d, el valor de i es: %d\n",
+               getpid(), getppid(),b);
+        }
+    }
+  
+    else {
+        pid1 = fork();
+        if (pid1 == 0) {
+            int b=0;
+            for (int i = 0; i <= 5; i++)
+            {    
+                b+=i;
+                printf("child[2] --> pid = %d and ppid = %d, el valor de i es: %d\n",
+                getpid(), getppid(),b);
+            }
+        }
+        else {
+            wait(NULL);
+            pid2 = fork();
+            if (pid2 == 0) {
+                int b=0;
+                for (int i = 0; i <= 5; i++)
+                {
+                    b+=i;
+                    printf("child[3] --> pid = %d and ppid = %d, el valor de i es: %d\n",
+                    getpid(), getppid(),b);
+                }
+            }
+
+            else {
+                
+                printf("parent --> pid = %d\n", getpid());
+            }
+        }
+    }
+  
+    return 0;
+}
